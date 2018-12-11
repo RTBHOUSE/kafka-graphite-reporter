ac07588d6843b19176668efd307377a344a53966
Tomasz Ulinski <tomasz.ulinski@rtbhouse.com>
2018-12-11 10:19:52 +0100

    Fix GaugeAdapter (ConcurrentModificationException)

--- src/main/java/com/rtbhouse/reporter/GaugeAdapter.java
+++ src/main/java/com/rtbhouse/reporter/GaugeAdapter.java
@@ -1,10 +1,15 @@
 package com.rtbhouse.reporter;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import org.apache.kafka.common.metrics.KafkaMetric;
+import org.slf4j.Logger;
 
 import com.codahale.metrics.Gauge;
 
 public class GaugeAdapter implements Gauge<Double> {
+
+    private static final Logger logger = getLogger(GaugeAdapter.class);
 
     private final KafkaMetric metric;
 
@@ -14,9 +19,13 @@ public class GaugeAdapter implements Gauge<Double> {
 
     @Override
     public Double getValue() {
-        double value = this.metric.value();
-
-        return Double.isNaN(value) ? 0. : value;
+        try {
+            double value = (double) this.metric.metricValue();
+            return Double.isNaN(value) ? 0. : value;
+        } catch (ClassCastException e) {
+            logger.warn("Cannot cast (return 0.0)", e);
+            return 0.;
+        }
     }
 
 }
